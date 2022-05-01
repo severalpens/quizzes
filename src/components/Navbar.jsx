cc70feabbf7c54cae778319de3642a0c38a531bf
Severalpens <Paul.Collins@ibm.com>
2022-05-02 00:13:34 +1000

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,16 +14,10 @@ function Navbar(props) {
     <div id="navbar" className="border border-black  flex justify-between  m-4  px-4  rounded-lg mx-10">
       <div id="navbar-main-menu" className="columns-2 p-6 flex">
         <NavLink id="navlink-home" className="font-bold p-6 navlink" to="/">
-          Time Tracker
+          Quizzes
         </NavLink>
-        <NavLink id="navlink-projects" className="p-6" to="/projects">
-          Projects
-        </NavLink>
-        <NavLink id="navlink-tasks" className="p-6" to="/tasks">
-          Tasks
-        </NavLink>
-        <NavLink id="navlink-entries" className="p-6" to="/entries">
-          Entries
+        <NavLink id="navlink-projects" className="p-6" to="/azure/questions">
+          Azure
         </NavLink>
       </div>
       <div id="navbar-auth-menu" className="p-6 columns-2">
